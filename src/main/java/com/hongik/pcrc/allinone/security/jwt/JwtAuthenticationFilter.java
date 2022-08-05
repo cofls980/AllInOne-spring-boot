29218cb7948cc933db04f4bbc5b6c4849200a876
cofls980 <qawse980@gmail.com>
2022-08-06 04:44:48 +0900

    [FEAT] authentication with spring security

---
+++ src/main/java/com/hongik/pcrc/allinone/security/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,40 @@
+package com.hongik.pcrc.allinone.security.jwt;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends GenericFilterBean {
+
+    private final JwtProvider jwtProvider;
+
+    /*
+    * Jwt 토큰의 인증 정보를 현재 실행 중인 SecurityContext에 저장하는 역할을 수행
+    * request의 header에서 토큰을 가져오고 유효성 체크 후 해당 토큰이 유효하다면 Security Context에 인증 정보 저장
+    * */
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        // 헤더에서 JWT 받아온다.
+        String token = jwtProvider.resolveToken((HttpServletRequest) request);
+
+        // 유효한 토큰인지 확인한다.
+        if (token != null && jwtProvider.validateJwtToken(request, token)) {
+            // 토큰이 유효하면 토큰으로부터 유저 정보 받아온다.
+            Authentication authentication = jwtProvider.getAuthentication(token);
+            // SecurityContext에 Authentication 객체를 저장한다.
+            SecurityContextHolder.getContext().setAuthentication(authentication);//세션에서 계속 사용하기 위해 securityContext에 Authentication 등록
+        }
+        //refresh로 access token 생성하는 과정 추가
+        chain.doFilter(request, response);
+    }
+}
---
+++ src/main/java/com/hongik/pcrc/allinone/security/service/CustomUserDetailService.java
@@ -0,0 +1,33 @@
+package com.hongik.pcrc.allinone.security.service;
+
+import com.hongik.pcrc.allinone.auth.application.domain.Auth;
+import com.hongik.pcrc.allinone.auth.infrastructure.persistance.mysql.repository.AuthEntityRepository;
+import com.hongik.pcrc.allinone.exception.AllInOneException;
+import com.hongik.pcrc.allinone.exception.MessageType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService { // UserDetailsService 클래스 커스텀
+    /*
+    * DB에서 유저의 정보를 조회하는 역할 수행
+    * UserDetailsService 인터페이스에서 DB에서 유저정보를 불러오는 중요한 메소드는 loadUserByUsername() 메서드이다.
+    * */
+    private final AuthEntityRepository authEntityRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws AuthenticationException {
+
+        Auth auth = authEntityRepository.findById(email).get().toAuth();
+
+        if (auth == null) {
+            throw new AllInOneException(MessageType.UsernameOrPasswordNotFound);
+        }
+
+        return auth;
+    }
+}
