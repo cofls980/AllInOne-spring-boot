4e3a30bca973bb721e1c27d0d982ee2a4fc27f10
cofls980 <qawse980@gmail.com>
2022-11-30 20:33:53 +0900

    [FEAT] delete scrap info when withdraw service

--- src/main/java/com/hongik/pcrc/allinone/cafe_map/infrastructure/persistance/mysql/repository/CafeMapMapperRepository.java
+++ src/main/java/com/hongik/pcrc/allinone/cafe_map/infrastructure/persistance/mysql/repository/CafeMapMapperRepository.java
@@ -25,4 +25,5 @@ public interface CafeMapMapperRepository {
     void createScrap(@Param("cafe_id") int cafe_id, @Param("user_id") String user_id);
     void deleteScrap(@Param("scrap_id") int scrap_id);
     List<HashMap<String, Object>> getScrap(@Param("user_id") String user_id);
+    void deleteAllUserScrap(@Param("user_id") String user_id);
 }
